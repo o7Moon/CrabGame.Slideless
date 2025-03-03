ff1038e40444671df827bd390cc22ee5a4d8dc97
o7Moon <0167moon@gmail.com>
2025-03-03 16:57:45 -0600

    0.1

--- Plugin.cs
+++ Plugin.cs
@@ -2,7 +2,7 @@ using BepInEx;
 using BepInEx.IL2CPP;
 using HarmonyLib;
 
-namespace crabgame_mod_template
+namespace slideless
 {
     [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
     public class Plugin : BasePlugin
@@ -12,6 +12,15 @@ namespace crabgame_mod_template
             Harmony.CreateAndPatchAll(typeof(Plugin));
 
             Log.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+        [HarmonyPatch(typeof(OnlinePlayer), nameof(OnlinePlayer.SetAnimation))]
+        [HarmonyPostfix]
+        public static void onCrouch(OnlinePlayer __instance, MonoBehaviourPublicObVeSiVeRiSiAnVeanTrUnique.EnumNPublicSealedvaCrMe3vUnique __0, bool __1, float __2){
+            if (__1 && __0 == MonoBehaviourPublicObVeSiVeRiSiAnVeanTrUnique.EnumNPublicSealedvaCrMe3vUnique.Crouch) {
+                UnityEngine.Debug.Log("AAAAAA");
+                var id = __instance.field_Private_MonoBehaviourPublicCSstReshTrheObplBojuUnique_0.steamProfile.m_SteamID;
+                GameServer.PlayerDied(id,id,UnityEngine.Vector3.down);
+            }
         }
     }
 }
